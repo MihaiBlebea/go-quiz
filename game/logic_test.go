1885d1b4d4c3ad505a33d6ab0b928e6bdd71584b
Mihai Blebea <mihai@getchip.uk>
2020-09-01 20:43:35 +0100

    Adds timeout duration test

--- game/logic_test.go
+++ game/logic_test.go
@@ -2,6 +2,7 @@ package game
 
 import (
 	"testing"
+	"time"
 
 	"github.com/MihaiBlebea/go-quiz/player"
 	"github.com/MihaiBlebea/go-quiz/quiz"
@@ -29,5 +30,33 @@ func TestGameCanBeWon(t *testing.T) {
 
 	if score != 2 {
 		t.Errorf("Expected score %d, got score %d", 2, score)
+	}
+}
+
+func TestGameExpires(t *testing.T) {
+	quiz, err := buildQuiz()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	player := player.Computer{Answers: []string{"London", "15"}}
+
+	start := time.Now()
+	gameService := New(0, *quiz, &player)
+	score, err := gameService.Run()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	duration := int(time.Since(start).Seconds())
+
+	if duration > 0 {
+		t.Errorf("Expected duration to be %d, got score %d", 0, duration)
+	}
+
+	if score == 2 {
+		t.Errorf("Expected score to not be %d, got score %d", 2, score)
 	}
 }
